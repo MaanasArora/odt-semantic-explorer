c5a22a3eae139328a0aa2018ff38a4a88410aa52
MaanasArora <maanasarora23@gmail.com>
2024-07-27 19:52:19 -0400

    Update distance matrix to use cosine similarity

--- api/algorithm/model.py
+++ api/algorithm/model.py
@@ -1,3 +1,5 @@
+from time import time
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -16,6 +18,8 @@ def preprocess_column(col):
 
 
 def fit_model(columns):
+    start = time()
+
     print("Preprocessing columns")
     columns = map(preprocess_column, columns)
 
@@ -24,13 +28,19 @@ def fit_model(columns):
     X = vectorizer.fit_transform(tqdm(columns)).toarray()
 
     print("Creating distance matrix")
-    X = np.dot(X, X.T)
-    X = 1 - X / np.max(X)
+    X = np.dot(X, X.T) / np.linalg.norm(X, axis=1) / np.linalg.norm(X, axis=1)[:, None]
+    X = 1 - X
 
     print("Clustering")
     model = AgglomerativeClustering(
-        n_clusters=None, metric="precomputed", linkage="single"
+        n_clusters=None,
+        metric="precomputed",
+        linkage="single",
+        distance_threshold=0.35,
     )
     clusters = model.fit_predict(X)
+
+    end = time()
+    print(f"Time elapsed: {end - start:.2f}s")
 
     return clusters.tolist()
